3e084c27a24dd48a4f2ac3c7a1a8c56886a199f8
Abhi241296 <abhishekhaksar35@gmail.com>
2020-01-31 01:47:18 -0700

    Rollaballassignment

---
+++ Roll a Ball/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject Player;
+    private Vector3 ofst;
+
+    void Start()
+    {
+        ofst = transform.position - Player.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = Player.transform.position + ofst;
+    }
+}
---
+++ Roll a Ball/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float sp = 5;
+    private Rigidbody rb;
+    private int cnt;
+    public Text cnttxt;
+    public Text wintxt;
+    
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        cnt = 0;
+        SetCount();
+        wintxt.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float mH = Input.GetAxis("Horizontal");
+        float mV = Input.GetAxis("Vertical");
+        Vector3 move = new Vector3(mH, 0.0f, mV);
+        rb.AddForce(move*5);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("tiles"))
+        {
+            other.gameObject.SetActive(false);
+            cnt = cnt + 1;
+            SetCount();
+        }
+    }
+
+    void SetCount()
+    {
+        cnttxt.text = "Score: " + cnt.ToString();
+        if(cnt >=9)
+        {
+            wintxt.text = "Congratulations!";
+        }
+    }
+}
